6d9dba73adca2a66bdd611c3051fa45dea35418a
Evelin Mara <evanzselin.que@gmail.com>
2020-02-06 10:16:21 +0100

    Fixing reducers for search movie action types

--- movie-app/src/app/store/movie/reducer/movie.reducer.ts
+++ movie-app/src/app/store/movie/reducer/movie.reducer.ts
@@ -1,18 +1,19 @@
 import { Movie } from 'src/app/models/Movie';
 import { MovieAction, MovieActionTypes } from '../actions/movie.actions';
+import { DetailedMovie } from 'src/app/models/DetailedMovie';
 
 export interface MovieState {
   readonly title: string;
-  readonly movies: Movie[];
   readonly isLoading: boolean;
   readonly errorMessage: string | null;
+  readonly movies: Movie[];
 }
 
 const initialState: MovieState = {
   title: '',
-  movies: [],
   isLoading: false,
   errorMessage: null,
+  movies: [],
 };
 
 export function MovieReducer(
@@ -21,11 +22,27 @@ export function MovieReducer(
 ): MovieState {
   switch (action.type) {
     case MovieActionTypes.SEARCH_MOVIES:
-      return { ...state, title: action.title, isLoading: true };
+      return {
+        ...state,
+        title: action.title,
+        isLoading: true,
+        errorMessage: null,
+        movies: [],
+      };
     case MovieActionTypes.SEARCH_MOVIES_SUCCES:
-      return {...state, movies: action.movies, isLoading: false};
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: null,
+        movies: action.movies,
+      };
     case MovieActionTypes.SEARCH_MOVIES_FAIL:
-      return {...state,  movies: [], errorMessage: action.error.message, isLoading: false};
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: action.error.message,
+        movies: [],
+      };
     default:
       return state;
   }
