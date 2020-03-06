01f1767cfe7da1c4ab2bd7752cd764ff5915624c
Evelin Mara <evanzselin.que@gmail.com>
2020-03-06 10:03:03 +0100

    Tests(AuthReducer) refactoring test cases

--- movie-app/src/app/auth/store/reducer/auth.reducer.spec.ts
+++ movie-app/src/app/auth/store/reducer/auth.reducer.spec.ts
@@ -17,6 +17,13 @@ import { Credentials } from '../../models/credentials.model';
 import { User } from '../../models/user.model';
 
 describe('Auth Reducer', () => {
+  const credentials: Credentials = {
+    email: 'user@domain.com',
+    password: 'pwd'
+  };
+  const user: User = { id: 'id', email: credentials.email };
+  const error = new Error('an error occured');
+
   describe('undefined action', () => {
     it('should return the default state', () => {
       const action = createAction('NOOP');
@@ -25,145 +32,126 @@ describe('Auth Reducer', () => {
     });
   });
 
-  describe(AuthActionTypes.AUTH_RESET, () => {
+  describe(`${AuthActionTypes.AUTH_RESET}, ${AuthActionTypes.AUTH_SIGN_OUT_SUCCESS}`, () => {
     it('should return the default state', () => {
-      const action = reset;
-      const result = reducer(undefined, action);
-      expect(result).toEqual({
-        isLoading: false,
+      const state: AuthState = {
+        isLoading: true,
+        errorMessage: error.message,
+        user
+      };
+
+      const actionReset = reset();
+      const resultReset = reducer(state, actionReset);
+
+      const actionSignOutSuccess = signOutSuccess();
+      const resultSignOutSuccess = reducer(state, actionSignOutSuccess);
+
+      [resultReset, resultSignOutSuccess].forEach(result =>
+        expect(result).toEqual({
+          isLoading: false,
+          errorMessage: undefined,
+          user: undefined
+        })
+      );
+    });
+  });
+
+  describe(`${AuthActionTypes.AUTH_SIGN_UP}, ${AuthActionTypes.AUTH_SIGN_IN}`, () => {
+    it('should toggle isLoading in state', () => {
+      const state: AuthState = {
+        isLoading: true,
         errorMessage: undefined,
         user: undefined
-      });
+      };
+
+      const actionSignUp = signUp({ credentials });
+      const resultSignUp = reducer(state, actionSignUp);
+
+      const actionSignIn = signIn({ credentials });
+      const resultSignIn = reducer(state, actionSignIn);
+
+      [resultSignUp, resultSignIn].forEach(result =>
+        expect(result).toEqual({
+          ...state,
+          isLoading: true
+        })
+      );
     });
   });
-
-  const credentials: Credentials = {
-    email: 'user@domain.com',
-    password: 'pwd'
-  };
-
-  describe(AuthActionTypes.AUTH_SIGN_UP, () => {
-    it('should toggle isLoading in state', () => {
-      const action = signUp({ credentials });
-      const result = reducer(initialState, action);
-      expect(result).toEqual({
-        ...initialState,
-        isLoading: true
-      });
-    });
-  });
-
-  const signUpState: AuthState = {
-    isLoading: true,
-    errorMessage: undefined,
-    user: undefined
-  };
 
   describe(AuthActionTypes.AUTH_SIGN_UP_SUCCES, () => {
     it('it should toggle off isLoading', () => {
+      const state: AuthState = {
+        isLoading: true,
+        errorMessage: undefined,
+        user: undefined
+      };
       const action = signUpSuccess({ credentials });
-      const result = reducer(signUpState, action);
+      const result = reducer(state, action);
       expect(result).toEqual({
-        ...signUpState,
+        ...state,
         isLoading: false
       });
     });
   });
 
-  describe(AuthActionTypes.AUTH_SIGN_UP_FAIL, () => {
-    it('it should toggle off isLoading and update error message and user in state', () => {
-      const error = new Error('an error occured');
-      const action = signUpFail({ error });
-      const result = reducer(signUpState, action);
-      expect(result).toEqual({
-        ...signUpState,
-        isLoading: false,
-        errorMessage: error.message
-      });
-    });
-  });
-
-  describe(AuthActionTypes.AUTH_SIGN_IN, () => {
-    it('should toggle isLoading in state', () => {
-      const action = signIn({ credentials });
-      const result = reducer(initialState, action);
-      expect(result).toEqual({
-        ...initialState,
-        isLoading: true
-      });
-    });
-  });
-
-  const signInState: AuthState = {
-    isLoading: true,
-    errorMessage: undefined,
-    user: undefined
-  };
-
-  const user: User = { id: 'id', email: credentials.email };
-
   describe(AuthActionTypes.AUTH_SIGN_IN_SUCCES, () => {
     it('it should toggle off isLoading and update user in state', () => {
+      const state: AuthState = {
+        isLoading: true,
+        errorMessage: undefined,
+        user
+      };
       const action = signInSuccess({ user });
-      const result = reducer(signInState, action);
+      const result = reducer(state, action);
       expect(result).toEqual({
-        ...signUpState,
+        ...state,
         isLoading: false,
         user
       });
     });
   });
 
-  describe(AuthActionTypes.AUTH_SIGN_IN_FAIL, () => {
-    it('it should toggle off isLoading and update error message and user in state', () => {
-      const error = new Error('an error occured');
-      const action = signInFail({ error });
-      const result = reducer(signInState, action);
-      expect(result).toEqual({
-        ...signUpState,
-        isLoading: false,
-        errorMessage: error.message
-      });
+  describe(`${AuthActionTypes.AUTH_SIGN_UP_FAIL}, ${AuthActionTypes.AUTH_SIGN_IN_FAIL}, ${AuthActionTypes.AUTH_SIGN_OUT_FAIL}`, () => {
+    it('it should toggle off isLoading and remove error message from state', () => {
+      const state: AuthState = {
+        isLoading: true,
+        errorMessage: undefined,
+        user: undefined
+      };
+
+      const actionSignUpFail = signUpFail({ error });
+      const resultSignUpFail = reducer(state, actionSignUpFail);
+
+      const actionSignInFail = signInFail({ error });
+      const resultSignInFail = reducer(state, actionSignInFail);
+
+      const actionSignOutFail = signOutFail({ error });
+      const resultSignOutFail = reducer(state, actionSignOutFail);
+
+      [resultSignUpFail, resultSignInFail, resultSignOutFail].forEach(result =>
+        expect(result).toEqual({
+          ...state,
+          isLoading: false,
+          errorMessage: error.message
+        })
+      );
     });
   });
-
-  const signedInState: AuthState = {
-    isLoading: false,
-    errorMessage: undefined,
-    user
-  };
 
   describe(AuthActionTypes.AUTH_SIGN_OUT, () => {
-    it('it should toggle isLoading and update error message in state', () => {
+    it('it should toggle on isLoading and remove error message from state', () => {
+      const state: AuthState = {
+        isLoading: false,
+        errorMessage: error.message,
+        user: undefined
+      };
       const action = signOut();
-      const result = reducer(signedInState, action);
+      const result = reducer(state, action);
       expect(result).toEqual({
-        ...signedInState,
+        ...state,
         isLoading: true,
         errorMessage: undefined
-      });
-    });
-  });
-
-  describe(AuthActionTypes.AUTH_SIGN_OUT_SUCCESS, () => {
-    it('it should return the default state', () => {
-      const action = signOutSuccess();
-      const result = reducer(signInState, action);
-      expect(result).toEqual({
-        ...initialState
-      });
-    });
-  });
-
-  describe(AuthActionTypes.AUTH_SIGN_OUT_FAIL, () => {
-    it('it should toggle off isLoading and update error message in state', () => {
-      const error = new Error('an error occured');
-      const action = signOutFail({ error });
-      const result = reducer(signedInState, action);
-      expect(result).toEqual({
-        ...signedInState,
-        isLoading: false,
-        errorMessage: error.message
       });
     });
   });
