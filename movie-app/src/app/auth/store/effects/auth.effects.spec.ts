77df9e42fef42a0c5903414d0f619bec8dbe3129
Evelin Mara <evanzselin.que@gmail.com>
2020-03-13 16:00:07 +0100

    Tests(AuthEffects) sign in cases added

--- movie-app/src/app/auth/store/effects/auth.effects.spec.ts
+++ movie-app/src/app/auth/store/effects/auth.effects.spec.ts
@@ -9,6 +9,7 @@ import { Actions } from '@ngrx/effects';
 import { hot, cold } from 'jest-marbles';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Credentials } from '../../models/credentials.model';
+import { User } from '../../models/user.model';
 
 const credentials: Credentials = {
   email: 'user@domain.com',
@@ -65,18 +66,55 @@ describe('AuthEffects', () => {
   });
 
   describe('signUp service call, fail', () => {
-    it(`should return an action of type ${AuthStore.signUpFail}`, () => {
+    it(`should return an action of type ${AuthStore.signUpFail.type} with error`, () => {
       const action = AuthStore.signUp({ credentials });
+      const outcome = AuthStore.signUpFail({ error });
 
       actions$ = hot('-a', { a: action });
 
       const response$ = cold('-#', {}, error);
       authService.signUp = jest.fn(() => response$);
 
-      const outcome = AuthStore.signUpFail({ error });
       const expected$ = cold('--b', { b: outcome });
-
       expect(effects.signUp$).toBeObservable(expected$);
+    });
+  });
+
+  describe('signIn service call, ', () => {
+    it(`should return an action of type ${AuthStore.signInSuccess} with user`, () => {
+      const user: User = { id: 'id', email: 'user@domain.com' };
+
+      const action = AuthStore.signIn({ credentials });
+      const outcome = AuthStore.signInSuccess({ user });
+
+      actions$ = hot('-a', { a: action });
+
+      const metadata = {
+        user: { uid: user.id, email: user.email },
+        returnUrl: undefined
+      };
+      const response$ = cold('-a', {
+        a: { user: metadata.user, returnUrl: metadata.returnUrl }
+      });
+      authService.signIn = jest.fn(() => response$);
+
+      const expected$ = cold('--b', { b: outcome });
+      expect(effects.signIn$).toBeObservable(expected$);
+    });
+  });
+
+  describe('signIn service call, fail', () => {
+    it(`should return an action of type ${AuthStore.signInFail.type} with error`, () => {
+      const action = AuthStore.signIn({ credentials });
+      const outcome = AuthStore.signInFail({ error });
+
+      actions$ = hot('-a', { a: action });
+
+      const response$ = cold('-#', {}, error);
+      authService.signIn = jest.fn(() => response$);
+
+      const expected$ = cold('--b', { b: outcome });
+      expect(effects.signIn$).toBeObservable(expected$);
     });
   });
 });
