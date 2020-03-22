c6bdc5a3ca1ba9660c5297580c5ad829f7f9c0d2
Evelin Mara <evanzselin.que@gmail.com>
2020-03-22 19:55:09 +0100

    Update(OmdbInterceptor) updated for snack bar error handling

--- movie-app/src/app/core/interceptors/omdb.interceptor.ts
+++ movie-app/src/app/core/interceptors/omdb.interceptor.ts
@@ -2,30 +2,51 @@ import {
   HttpInterceptor,
   HttpEvent,
   HttpRequest,
-  HttpHandler
+  HttpHandler,
+  HttpResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store';
+import { notify } from '../store/snack-bar';
 
 @Injectable()
 export class OmdbInterceptor implements HttpInterceptor {
+  constructor(private store: Store<AppState>) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    let request = req;
     const omdbApiUrl = environment.omdb.url;
 
     if (req.url.startsWith(omdbApiUrl)) {
-      const request = req.clone({
+      request = req.clone({
         params: req.params.set(
           environment.omdb.paramApiKey,
           environment.omdb.apiKey
         )
       });
-      return next.handle(request);
     }
 
-    return next.handle(req);
+    return next.handle(request).pipe(
+      tap(event => {
+        if (event instanceof HttpResponse) {
+          const message = event.body.Error;
+
+          if (message) {
+            this.store.dispatch(
+              notify({ message, cssClass: 'snack-bar-error' })
+            );
+          }
+
+          return of(event);
+        }
+      })
+    );
   }
 }
