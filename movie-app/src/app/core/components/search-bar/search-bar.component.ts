205a8640016925b62d3ddfb88c8c8d9af61818a0
Evelin Mara <evanzselin.que@gmail.com>
2020-03-23 12:34:41 +0100

    Feature(SearchBar) Reset search bar after search

--- movie-app/src/app/core/components/search-bar/search-bar.component.ts
+++ movie-app/src/app/core/components/search-bar/search-bar.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../store';
 import { search } from '../../store/movie';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
@@ -10,6 +11,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
+  title = new FormControl('');
+
   constructor(
     private store: Store<AppState>,
     private ngZone: NgZone,
@@ -18,13 +21,17 @@ export class SearchBarComponent implements OnInit {
 
   ngOnInit() {}
 
-  onSearch(title: string) {
+  onSearch() {
+    const title = this.title.value;
+    if (!title) {
+      return;
+    }
+
+    this.title.reset();
     this.store.dispatch(search({ title }));
 
     this.ngZone.run(() => {
-      this.ngZone.run(() => {
-        this.router.navigate(['/movies']);
-      });
+      this.router.navigate(['/movies']);
     });
   }
 }
