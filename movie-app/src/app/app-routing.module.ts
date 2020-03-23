f96ef5d01423b4d43a7a8e62004786840c8cbceb
Evelin Mara <evanzselin.que@gmail.com>
2020-03-23 14:11:11 +0100

    Feature(Movies) Guard added

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { MoviesComponent } from './core/containers/movies/movies.component';
 import { WatchListComponent } from './core/containers/watch-list/watch-list.component';
 import { WatchListGuard } from './core/guards/watch-list.guard';
+import { MoviesGuard } from './core/guards/movies.guard';
 
 const routes: Routes = [
   { path: 'movies/:id', component: MovieComponent },
   {
     path: 'movies',
-    component: MoviesComponent
+    component: MoviesComponent,
+    canActivate: [MoviesGuard]
   },
   {
     path: 'watchList',
