86f91575c8c6cc9b90b27cdbd0f5496eddf076e1
Evelin Mara <evanzselin.que@gmail.com>
2020-04-06 08:22:42 +0200

    Featre(WatchList) Added auth related watch list effects

--- movie-app/src/app/watch-list/store/effects/watch-list.effects.ts
+++ movie-app/src/app/watch-list/store/effects/watch-list.effects.ts
@@ -6,11 +6,42 @@ import { of } from 'rxjs';
 import { WatchList2Service } from '../../services/watch-list-2.service';
 
 import * as WatchListActions from '../actions/watch-list.actions';
+import * as AuthActions from '../../../auth/store/actions/auth.actions';
 import * as SnackBarActions from '../../../core/store/snack-bar/actions/snack-bar.actions';
 
 @Injectable()
 export class WatchListEffects {
   constructor(private actions$: Actions, private service: WatchList2Service) {}
+
+  loadOnSignInSuccess$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.signInSuccess),
+      map(() => WatchListActions.load()),
+      catchError((error) =>
+        of(
+          SnackBarActions.notify({
+            message: error,
+            cssClass: 'snack-bar-error',
+          })
+        )
+      )
+    )
+  );
+
+  resetOnSignOutSuccess$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.signOutSuccess),
+      map(() => WatchListActions.reset()),
+      catchError((error) =>
+        of(
+          SnackBarActions.notify({
+            message: error,
+            cssClass: 'snack-bar-error',
+          })
+        )
+      )
+    )
+  );
 
   load$ = createEffect(() =>
     this.actions$.pipe(
