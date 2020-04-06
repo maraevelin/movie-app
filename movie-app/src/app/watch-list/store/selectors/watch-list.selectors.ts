cb11a9091223277a3c00890c7641d2fbcdca251f
Evelin Mara <evanzselin.que@gmail.com>
2020-04-06 08:20:22 +0200

    Fix(WatchListSelectors) select data in array format

--- movie-app/src/app/watch-list/store/selectors/watch-list.selectors.ts
+++ movie-app/src/app/watch-list/store/selectors/watch-list.selectors.ts
@@ -1,6 +1,7 @@
 import { AppState } from 'src/app/core/store';
 import { createSelector } from '@ngrx/store';
 import * as WatchListStore from '../';
+import { WatchList2Data } from '../reducer/watch-list.reducer';
 
 export const selectWatchList = (state: AppState) => state.watchList;
 
@@ -14,5 +15,10 @@ export const selectErrorMessage = createSelector(
 );
 export const selectData = createSelector(
   selectWatchList,
-  (state: WatchListStore.WatchListState) => state.data
+  (state: WatchListStore.WatchListState) => {
+    return Object.values(state.data).reduce((datas: WatchList2Data[], data) => {
+      datas.push(data);
+      return datas;
+    }, []);
+  }
 );
