006a5956cc2b374bf807a2911b0c94717f9fd7cd
Evelin Mara <evanzselin.que@gmail.com>
2020-04-10 17:00:14 +0200

    Fix(WatchList) Deleted previous state

--- movie-app/src/app/watch-list/store/watch-list.state.ts
+++
@@ -1,8 +0,0 @@
-import { WatchListCollection } from 'src/app/watch-list/models/watch-list-collection.model';
-
-export interface WatchListState {
-  isLoading: boolean;
-  isUpdated: boolean;
-  movies: WatchListCollection;
-  errorMessage: string | undefined;
-}
