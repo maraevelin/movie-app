725a140820bb9c0cecf74064e4189e57dbd3354e
Evelin Mara <evanzselin.que@gmail.com>
2020-04-10 18:24:16 +0200

    Fix(WatchList) Removed watch list from non-WatchList modules

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { WatchList2Component } from './shared/components/watch-list2/watch-list2.component';
 
-const routes: Routes = [
-  {
-    path: 'watchList2',
-    component: WatchList2Component,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movie-app/src/app/shared/shared.module.ts
+++ movie-app/src/app/shared/shared.module.ts
@@ -4,14 +4,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { MaterialDesignModule } from '../material-design/material-design.module';
 import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
-import { WatchList2Component } from './components/watch-list2/watch-list2.component';
 
 @NgModule({
-  declarations: [
-    SpinnerComponent,
-    ConfirmationDialogComponent,
-    WatchList2Component,
-  ],
+  declarations: [SpinnerComponent, ConfirmationDialogComponent],
   imports: [CommonModule, MaterialDesignModule, ReactiveFormsModule],
   exports: [SpinnerComponent, ConfirmationDialogComponent],
 })
