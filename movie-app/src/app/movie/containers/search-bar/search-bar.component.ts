b9da592a690959f27aeecacf7bf17e281c522f64
Evelin Mara <evanzselin.que@gmail.com>
2020-04-11 09:15:23 +0200

    Fix(SearchBar) Fixed subscription errors and namings

--- movie-app/src/app/movie/containers/search-bar/search-bar.component.ts
+++ movie-app/src/app/movie/containers/search-bar/search-bar.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../core/store';
 import { search, selectTitle } from '../../store/movie';
-import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-bar',
@@ -13,37 +13,30 @@ import { Observable } from 'rxjs';
 })
 export class SearchBarComponent implements OnInit {
   title = new FormControl('');
+
   title$: Observable<string>;
 
-  constructor(
-    private store: Store<AppState>,
-    private ngZone: NgZone,
-    private router: Router
-  ) {
+  constructor(private store: Store<AppState>) {
     this.title$ = this.store.select(selectTitle);
   }
 
   ngOnInit() {}
 
   onSearch() {
-    const title = this.title.value.trim();
-    if (!title) {
+    const searchedTitle = this.title.value.trim();
+    if (!searchedTitle) {
       return;
     }
 
     let isAlreadyShown = false;
-    this.title$.subscribe(
-      (t) => (isAlreadyShown = title.toLowerCase() === t.toLowerCase())
-    );
-    if (isAlreadyShown) {
-      return;
-    }
+    this.title$.pipe(first()).subscribe((lastTitle) => {
+      isAlreadyShown = searchedTitle.toLowerCase() === lastTitle.toLowerCase();
+      if (isAlreadyShown) {
+        return;
+      }
 
-    this.title.reset();
-    this.store.dispatch(search({ title }));
-
-    this.ngZone.run(() => {
-      this.router.navigate(['/movies']);
+      this.title.reset();
+      this.store.dispatch(search({ title: searchedTitle }));
     });
   }
 }
