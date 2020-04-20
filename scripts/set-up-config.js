be2c417305255d91a5bf03b50de2ac883426f233
Evelin Mara <evanzselin.que@gmail.com>
2020-04-20 21:56:42 +0200

    environment overwrite

--- scripts/set-up-config.js
+++ scripts/set-up-config.js
@@ -1,9 +1,10 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const environmentPath = path.join(__dirname, '../src/environments/environment.prod.ts');
+const environmentProdPath = path.join(__dirname, '../src/environments/environment.prod.ts');
+const environmentPath = path.join(__dirname, '../src/environments/environment.ts');
 
-const template = fs.readFileSync(environmentPath, 'utf8');
+const template = fs.readFileSync(environmentProdPath, 'utf8');
 
 const keys = ['omdbApiKey', 'firebaseApiKey', 'firebaseAuthDomain', 'firebaseDatabaseURL', 'firebaseProjectId', 'firebaseStorageBucket', 'firebaseMessagingSenderId', 'firebaseAppId', 'firebaseMeasurementId', 'firebaseUrl'];
 
@@ -11,5 +12,6 @@ const args = process.argv.slice(2);
 
 const result = keys.reduce((acc, key, index) => acc.replace(`{${key}}`, args[index]), template);
 
-fs.writeFileSync(environmentPath, result)
+fs.writeFileSync(environmentProdPath, result);
+fs.writeFileSync(environmentPath, result);
 
